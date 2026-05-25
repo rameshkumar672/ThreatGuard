8a536bc2454b79bc7be30aaae208a83ce75f9e18
Ramesh Kumar <ramesh969399@gmail.com>
2026-05-25 23:03:44 +0530

    push app

--- fronted/src/utils/api.js
+++ fronted/src/utils/api.js
@@ -24,7 +24,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://rameshkumar672-threatguard-backend.hf.space/api",
   headers: {
     "Content-Type": "application/json",
   },
